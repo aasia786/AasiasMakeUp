aa01c4342af52cd4849cbf67ab6b7d7d0cc0a771
vetienne <vincent.etienne1@external.engie.com>
2019-04-19 17:19:58 +0200

    Ajout de la liste avec fausses données

--- app/src/main/java/com/queen/aasiasmakeup/MainActivity.java
+++ app/src/main/java/com/queen/aasiasmakeup/MainActivity.java
@@ -1,33 +1,36 @@
 package com.queen.aasiasmakeup;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button btnQueen = findViewById(R.id.btn_second_ecran);
-
-        btnQueen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               launchSecondScreen();
-            }
-        });
-
-
-        String toto = "toto";
-    }
-
-    private void launchSecondScreen() {
-        Intent intent = new Intent(getBaseContext(), SecondActivity.class);
-        startActivity(intent);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        // use this setting to
+        // improve performance if you know that changes
+        // in content do not change the layout size
+        // of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        List<String> input = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            input.add("Test" + i);
+        }// define an adapter
+        mAdapter = new MakeUpAdapter(input);
+        recyclerView.setAdapter(mAdapter);
     }
 }
