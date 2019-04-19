3a7241f5c9cd91f954ec372d8a894d6da76e190f
Javaid <javaid@et.esiea.fr>
2019-04-19 15:25:35 +0200

    Ajout second activity

--- app/src/main/java/com/queen/aasiasmakeup/MainActivity.java
+++ app/src/main/java/com/queen/aasiasmakeup/MainActivity.java
@@ -1,7 +1,10 @@
 package com.queen.aasiasmakeup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnQueen = findViewById(R.id.btn_second_ecran);
+
+        btnQueen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               launchSecondScreen();
+            }
+        });
+
+
+        String toto = "toto";
+    }
+
+    private void launchSecondScreen() {
+        Intent intent = new Intent(getBaseContext(), SecondActivity.class);
+        startActivity(intent);
     }
 }
